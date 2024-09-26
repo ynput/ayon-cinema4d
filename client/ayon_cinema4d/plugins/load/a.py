f261d8863683e83e5eb63ce2a039ddc484dd79df
Roy Nieterau <roy_nieterau@hotmail.com>
2024-09-26 16:24:27 +0200

    Delete temporary dev files

--- client/ayon_cinema4d/plugins/load/a.py
+++
@@ -1,8 +0,0 @@
-
-from ayon_cinema4d.api import lib, pipeline, plugin
-
-# TODO: Remove debug reloading
-import importlib
-importlib.reload(lib)
-importlib.reload(pipeline)
-importlib.reload(plugin)
