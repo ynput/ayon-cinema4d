676d3c39591c64b7d2577a818da9aa4c7220bfd8
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-02 14:04:19 +0200

    bump version to '0.1.0+dev'

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "cinema4d"
 title = "Cinema4D"
-version = "0.1.0"
+version = "0.1.0+dev"
 
 # Name of client code directory imported in AYON launcher
 # - do not specify if there is no client code
