cbd3fd09d0121fdf4ba3fc06faec8b0ef640c99d
Roy Nieterau <roy_nieterau@hotmail.com>
2025-11-09 14:13:07 +0100

    Continued work on getting render outputs collected

--- client/ayon_cinema4d/plugins/publish/collect_render.py
+++ client/ayon_cinema4d/plugins/publish/collect_render.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 import attr
+import os
 import pyblish.api
 
 import clique
 
 from ayon_core.pipeline import publish
-from ayon_cinema4d.api import lib_renderproducts
+from ayon_cinema4d.api import lib, lib_renderproducts
 
 import c4d
 import c4d.documents
@@ -31,8 +32,14 @@ class Cinema4DRenderInstance(publish.RenderInstance):
 
 
 class CollectCinema4DRender(
-    publish.AbstractCollectRender, publish.ColormanagedPyblishPluginMixin
+    publish.AbstractCollectRender,
+    publish.ColormanagedPyblishPluginMixin
 ):
+    """
+    Each active render instance represents a `Take` inside Cinema4D. For this
+    take we will get its render settings and will compute the applicable
+    frame range and expected output files as well.
+    """
     order = pyblish.api.CollectorOrder + 0.1
     label = "Collect Render"
     hosts = ["cinema4d"]
@@ -42,10 +49,9 @@ class CollectCinema4DRender(
         current_file = context.data["currentFile"]
         version = context.data.get("version")
         project_entity = context.data["projectEntity"]
-
         doc: c4d.documents.BaseDocument = context.data["doc"]
         take_data = doc.GetTakeData()
-        instances = []
+        instances: list[Cinema4DRenderInstance] = []
         for inst in context:
             if not inst.data.get("active", True):
                 continue
@@ -58,8 +64,6 @@ class CollectCinema4DRender(
             take: c4d.modules.takesystem.BaseTake = (
                 inst.data["transientData"]["take"]
             )
-
-            # take_data.SetCurrentTake(take)
             render_data, base_take = take.GetEffectiveRenderData(take_data)
 
             # Get take name, resolution, frame range
@@ -67,8 +71,12 @@ class CollectCinema4DRender(
             resolution_width: int = int(render_data[c4d.RDATA_XRES])
             resolution_height: int = int(render_data[c4d.RDATA_YRES])
             pixel_aspect: float = float(render_data[c4d.RDATA_PIXELASPECT])
-            frame_start: int = int(render_data[c4d.RDATA_FRAMEFROM].GetFrame(fps))
-            frame_end: int = int(render_data[c4d.RDATA_FRAMETO].GetFrame(fps))
+            frame_start: int = int(
+                render_data[c4d.RDATA_FRAMEFROM].GetFrame(fps)
+            )
+            frame_end: int = int(
+                render_data[c4d.RDATA_FRAMETO].GetFrame(fps)
+            )
             step: int = int(render_data[c4d.RDATA_FRAMESTEP])
 
             instance_families = inst.data.get("families", [])
@@ -127,18 +135,30 @@ class CollectCinema4DRender(
 
     def get_expected_files(self, render_instance: Cinema4DRenderInstance):
         """Return expected output files from the render"""
+        # TODO: Support built-in standard, physical and viewport renderers
+        # TODO: Relative paths may need to be made absolute because otherwise
+        #  those paths will become relative to the PUBLISHED scenefile instead
+        #  of the WORKFILE?
+        # TODO: Separate into clearer isolated methods
+        #  self._collect_regular_image()
+        #  self._collect_multipass_image()
+        #  self._collect_video_posts(video_posts)
 
         instance: pyblish.api.Instance = render_instance.source_instance
         render_data: c4d.documents.RenderData = render_instance.renderData
         doc = render_data.GetDocument()
-        take: c4d.modules.takesystem.BaseTake = (
-            instance.data["transientData"]["take"]
-        )
 
         # From the Take and Render Data we find the correct output path,
         # whether it is multipass and what AOVs are enabled for the renderer.
         # Each output file is considered to be a "Render Product" similar to
         # USD terminology.
+        take: c4d.modules.takesystem.BaseTake = (
+            instance.data["transientData"]["take"]
+        )
+        renderer: int = render_data[c4d.RDATA_RENDERENGINE]
+        video_posts: list[c4d.documents.BaseVideoPost] = lib.get_siblings(
+            render_data.GetFirstVideoPost()
+        )
 
         # Debug log what take we're processing, etc.
         self.log.debug(f"Take: {take.GetName()}")
@@ -155,26 +175,43 @@ class CollectCinema4DRender(
             f"{render_instance.resolutionHeight}"
         )
 
-        # Main multi-pass output
-        token_path: str = render_data[c4d.RDATA_MULTIPASS_FILENAME]
+        # Save Regular image
+        save_image: bool = render_data[c4d.RDATA_SAVEIMAGE]
+        if save_image:
+            # token_path: str = render_data[c4d.RDATA_PATH]
+            self.log.warning(
+                "Saving regular image is enabled. "
+                "Currently the Collect Render implementation only supports "
+                "Multi-Pass Image. Please disable regular image saving."
+            )
+
+        # Save Multi-Pass image
+        save_multipass_image: bool = render_data[c4d.RDATA_MULTIPASS_SAVEIMAGE]
+        multipass_token_path: str = render_data[c4d.RDATA_MULTIPASS_FILENAME]
+        if not save_multipass_image:
+            self.log.warning(
+                "Saving Multi-Pass Image is disabled. It is currently the "
+                "only saving that Collect Render currently supports. "
+                "Please enable it. "
+            )
+            return []
 
         # If Multi-Layer File is enabled then the renderer will write into
         # a single file for all AOVs, except in some cases a renderer may write
         # into a separate file certain AOVs, like a Cryptomatte.
         # '$pass' becomes `unresolved` if multi-layer file is enabled but the
         # token is present in the output path.
-        is_multilayer_file = bool(render_data[c4d.RDATA_MULTIPASS_ENABLE])
+        multipass_enabled: bool = bool(render_data[c4d.RDATA_MULTIPASS_ENABLE])
+        multilayer_file: bool = render_data[c4d.RDATA_MULTIPASS_SAVEONEFILE]
 
         layer_name = "unresolved"       # $userpass if multi-layer file
         layer_type_name = "unresolved"  # $pass if multi-layer file
-        # if not is_multilayer_file:
+        # if not multipass_enabled:
         #     # Include all AOVs or multipasses separately
         #     pass
         # else:
         #     # Include just the main layer and assume everything ends in one
-        #     # file, with the exception of e.g. Redshift Cryptomatte
-        #     pass
-
+        #     # file, with exception of e.g. Redshift Cryptomatte pass
 
         # Get all frames
         files: list[str] = []
@@ -182,29 +219,34 @@ class CollectCinema4DRender(
             render_instance.frameStartHandle,
             render_instance.frameEndHandle + 1,
         ):
-            # TODO: Implement how get the correct output render path based
-            #  on the Cinema4D render settings
-            path = f"/path/to/file.{frame:0>4d}.exr"
-            files.append(path)
+            resolved_path = lib_renderproducts.resolve_filepath(
+                multipass_token_path,
+                doc=doc,
+                render_data=render_data,
+                layer_name=layer_name,
+                layer_type_name=layer_type_name,
+                take=take,
+                frame=frame
+            )
+            files.append(resolved_path)
 
         # Get take render data AOVs
-        renderer = render_data[c4d.RDATA_RENDERENGINE]
+        video_posts_names = ", ".join(vp.GetName() for vp in video_posts)
+        self.log.debug(f"  Video posts: {video_posts_names}")
+
+        # Support Redshift
         if renderer == redshift.VPrsrenderer:
-            video_post = render_data.GetFirstVideoPost()
-            self.log.info("Renderer is redshift.")
-            for aov in lib_renderproducts.iter_redshift_aovs(video_post):
-                # TODO: Support AOV light groups
-                pass
-        resolved_path = lib_renderproducts.resolve_filepath(
-            token_path,
-            doc,
-            render_data,
-            layer_name=layer_name,
-            layer_type_name=layer_type_name)
+            self.log.debug("Renderer is Redshift.")
+            self._collect_redshift_aovs(render_instance, video_posts)
 
-        self.log.debug(f"    Raw path: {token_path}")
-        self.log.debug(f"    Path: {resolved_path}")
+        # Set output dir because it is required for publish metadata to be
+        # written out and the publish job submission to succeed.
+        self.log.debug(
+            f"Collected output directory: {render_instance.outputDir}"
+        )
+        render_instance.outputDir = os.path.dirname(files[0])
 
+        # Products by AOV
         products = {
             # beauty
             "": files
@@ -222,3 +264,24 @@ class CollectCinema4DRender(
             self.log.debug(f"    {aov_name} files: {file_labels}")
 
         return [products]
+
+    def _collect_redshift_aovs(self, render_data, video_posts):
+        """Collect all Redshift AOVs"""
+        # Get Redshift Video Post
+        redshift_vp = next((
+            vp for vp in video_posts if vp.GetTypeName() == "Redshift"
+        ), None)
+        if not redshift_vp:
+            return
+
+        # If Global AOV mode is set to disabled, collect no AOV data
+        AOV_DISABLED = c4d.REDSHIFT_RENDERER_AOV_GLOBAL_MODE_DISABLE
+        if redshift_vp[c4d.REDSHIFT_RENDERER_AOV_GLOBAL_MODE] == AOV_DISABLED:
+            self.log.debug("Redshift Global AOV mode is disabled.")
+            return
+
+        for aov in lib_renderproducts.iter_redshift_aovs(redshift_vp):
+            # TODO: Support AOV light groups
+            # TODO: Actually return data
+            self.log.info(aov)
+            pass
